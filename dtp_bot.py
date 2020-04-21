61805a690f57e1629b6d2cd0a7613d0747b7ebd5
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2020-04-21 11:25:56 +0200

    Add role_clear command

--- dtp_bot.py
+++ dtp_bot.py
@@ -60,6 +60,7 @@ async def role_remove(ctx, *args):
     :param ctx: context
     :param args: role names
     """
+    print(args)
     user = ctx.message.author
     user_roles = user.roles
     roles_to_remove = list(
@@ -74,6 +75,16 @@ async def role_remove(ctx, *args):
         await ctx.send(f'Successfully removed roles {", ".join(args)}.')
     else:
         await ctx.send(f'No roles were removed from {user.name}.')
+
+
+@bot.command()
+async def role_clear(ctx):
+    """
+    Removes all roles from a user.
+    :param ctx: context
+    """
+    user_role_names = tuple(map(str, ctx.message.author.roles))
+    await role_remove(ctx, *user_role_names)
 
 # Messages
 print('Starting the bot...\nConstants:')
